c0793b56df5cf4ba5853c9a3b5ead66b9bbbef0b
Gabriel Lima <gabriellimasjc@gmail.com>
2023-01-10 17:52:19 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Refit;
+
+namespace ExemploConsumoAPI
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br/");
+
+                Console.WriteLine("Informe seu CEP: ");
+                string cepInformado = Console.ReadLine().ToString();
+
+                Console.WriteLine($"Consultando informações do CEP {cepInformado}");
+
+                var address = await cepClient.GetAddressAsync(cepInformado);
+
+                Console.WriteLine($"\n CEP: {address.Cep} " +
+                                  $"\n Rua: {address.Logradouro} " +
+                                  $"\n Bairro: {address.Bairro}" +
+                                  $"\n Cidade: {address.Localidade}" +
+                                  $"\n DDD: {address.Ddd}"); 
+                            
+
+            }catch (Exception ex)
+            {
+                Console.WriteLine($"Erro na consulta do CEP: {ex.Message}");
+            }
+        }
+    }
+}
